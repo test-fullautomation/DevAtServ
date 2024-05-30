eb1d50d72a273f0557ed68c17744144c7664d36b
hvthong <50081052+huavanthong@users.noreply.github.com>
2024-05-30 17:03:56 +0700

    Update start-devatserv.sh script

--- build/Linux/opt/devatserv/bin/start-devatserv.sh
+++ build/Linux/opt/devatserv/bin/start-devatserv.sh
@@ -1,4 +1,30 @@
 #!/bin/bash
 
 cd /opt/devatserv/share/start-services
-docker-compose up -d
+
+
+start_devatserv() {
+  echo "Starting DevAtServ's docker containers"
+
+  if ! docker compose up --remove-orphans -d; then
+    echo "Could not start. Check for errors above."
+    exit 1
+  fi
+
+  show_friendly_message
+}
+
+show_success_message() {
+  local ip_address
+  ip_address=$(hostname -I | awk '{print $1}')
+  cat <<EOF
+Device Automation Services App successfully deployed!
+You can access the website at http://$ip_address:15672 to access RabbitMQ Management
+---------------------------------------------------
+EOF
+}
+
+############################
+# Starting ...
+############################
+start_devatserv
