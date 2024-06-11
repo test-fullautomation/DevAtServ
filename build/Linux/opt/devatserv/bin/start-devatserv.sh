101848a8ab5c901a89f9fe5ebf4831a88c768a74
hvthong <50081052+huavanthong@users.noreply.github.com>
2024-06-11 09:45:41 +0700

    Remove the redundant files

--- build/Linux/opt/devatserv/bin/start-devatserv.sh
+++ build/Linux/opt/devatserv/bin/start-devatserv.sh
@@ -27,8 +27,6 @@ pre_check_installation() {
 install_gui_devatserv() {
   echo -e "${MSG_INFO} Starting DevAtServ's GUI"
 
-  sudo apt-get update
-
   # Install DevAtServ'GUI 
   if sudo dpkg -i /opt/share/applications/electron_1.0.0_amd64.deb; then
     echo -e "${MSG_DONE} DevAtServ's GUI has been installed successfully"
--- build/Linux/opt/devatserv/share/util/remove_gui_app.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Script to remove another app
-echo "Removing DevAtServ'GUI app..."
-sudo dpkg --remove electron
-
-exit 0
