71c5026f3c8408392e48602e4ecf8638a3826389
hvthong <50081052+huavanthong@users.noreply.github.com>
2024-08-07 17:23:46 +0700

    Implement the solution to install all services successfully.

--- install.sh
+++ install.sh
@@ -6,15 +6,15 @@ source ./util/format.sh
 source ./util/common.sh
 
 create_repos_directory() {
-  local repos_dir='./repos'
+	local repos_dir='./repos'
 
-  echo -e "${MSG_INFO} Creating repos directory for all DevAtServ service..."
+	echo -e "${MSG_INFO} Creating repos directory for all DevAtServ service..."
 
-  if [[ -e $repos_dir ]]; then
-    echo "Directory $repos_dir already exists."
-  else
-    mkdir -p "$repos_dir" || return
-  fi
+	if [[ -e $repos_dir ]]; then
+		echo "Directory $repos_dir already exists."
+	else
+		mkdir -p "$repos_dir" || return
+	fi
 }
 
 install_services () {
@@ -33,26 +33,29 @@ install_services () {
 }
 
 start_docker_compose() {
-  echo -e "${MSG_INFO} Starting the DevArtServ Docker containers"
+	echo -e "${MSG_INFO} Starting the DevArtServ Docker containers"
 
-  if ! docker compose >/dev/null 2>&1; then
-    echo "failed to find 'docker compose'"
-    exit 1
-  fi
+	if ! docker compose >/dev/null 2>&1; then
+		echo "failed to find 'docker compose'"
+		exit 1
+	fi
     
-  # Check for specific SUPPORT_SERVER value
-  if [ "$SUPPORT_SERVER" == "gitlab" ]; then
-    docker_compose_file="docker-compose.gitlab.yml"
-  elif [ "$SUPPORT_SERVER" == "github" ]; then
-    docker_compose_file="docker-compose.yml"
-  else
-	  errormsg "Docker compose file not found"
-  fi
+	docker_compose_files=("docker-compose.yml")
 
-  if ! docker compose -f "$docker_compose_file" up --remove-orphans -d; then
-    echo "Could not start. Check for errors above."
-    exit 1
-  fi
+	# Check for specific SUPPORT_SERVER value
+	if [ "$SUPPORT_SERVER" == "gitlab" ]; then
+  		docker_compose_files+=("docker-compose.debugboard.yml")
+	fi
+
+	compose_options=""
+	for file in "${docker_compose_files[@]}"; do
+		compose_options="$compose_options -f $file"
+	done
+
+	if ! docker-compose $compose_options up --remove-orphans -d; then
+		echo "Could not start. Check for errors above."
+		exit 1
+	fi
 }
 
 main() {
