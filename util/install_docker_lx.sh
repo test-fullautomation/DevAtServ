b66c6c813c23f6287b29d03e75935eba28ed665b
hvthong <50081052+huavanthong@users.noreply.github.com>
2024-08-07 13:30:43 +0700

    Fix error: docker installation is broken afterwards.

--- util/install_docker_lx.sh
+++ util/install_docker_lx.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-pre_install() {
+pre_remove() {
     sudo apt-get remove -y docker docker-engine docker.io containerd runc
     sudo apt-get update
+}
+
+pre_install() {
     sudo apt-get install -y apt-transport-https ca-certificates curl gnupg lsb-release
     curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
     echo \
@@ -13,25 +16,30 @@ pre_install() {
 
 # Install docker 
 install_docker() {
+    pre_remove
     pre_install
     sudo apt-get install -y docker-ce docker-ce-cli containerd.io
-    sudo apt-get install -y docker-compose-plugin
 }
 
 # Install docker 
 install_docker_compose() {
-    pre_install
     sudo apt-get install -y docker-compose-plugin
 }
 
 # Docker already installed or not
 if ! command -v docker &> /dev/null; then
     install_docker
+
+    if ! docker compose >/dev/null 2>&1; then
+        install_docker_compose
+    fi
 fi
 
 if ! docker compose >/dev/null 2>&1; then
+    pre_install
     install_docker_compose
 fi
+
 
 echo "Docker installation script completed successfully."
 
