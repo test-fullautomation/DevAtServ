3a2cd81f8128ccd72a1e8f77cf697321760adab2
hvthong <50081052+huavanthong@users.noreply.github.com>
2024-08-08 15:04:40 +0700

    Update the behavior to help us observe the output.

--- build/Linux/opt/devatserv/bin/start-devatserv.sh
+++ build/Linux/opt/devatserv/bin/start-devatserv.sh
@@ -101,6 +101,36 @@ You can access the website at http://localhost:15672 to access RabbitMQ Manageme
 EOF
 }
 
+exit_after_countdown() {
+
+  exit_after_time=10
+  elapsed_time=0
+  interval=1
+
+  # Hide cursor
+  tput civis
+
+  while [ $elapsed_time -lt $exit_after_time ]; do
+      # Wait for an interval
+      read -t $interval -n 1 input
+      if [ $? -eq 0 ]; then
+          echo -e "\nKey pressed. Exiting."
+          # Show the cursor
+          tput cnorm
+          exit 0
+      fi
+
+      elapsed_time=$((elapsed_time + interval))
+      remaining_time=$((exit_after_time - elapsed_time))
+      
+      # Update the remain time
+      printf "\rPress any key to exit early in %ds" $remaining_time
+  done
+
+  tput cnorm
+  exit 0
+}
+
 handle_error() {
   echo -e "${MSG_ERR} $1"
   read -p "Press Enter to continue..."
@@ -118,6 +148,8 @@ main() {
 
   start_devatserv || handle_error 'Error starting Docker containers'
   
+  exit_after_countdown || handle_error 'Error exiting'
+
   return 0
 }
 
