6349843fafe86b8c43db43383f8ae730aad7c8a5
hvthong <50081052+huavanthong@users.noreply.github.com>
2024-12-16 11:27:48 +0700

    Update more log installation.

--- util/install-app.bat
+++ util/install-app.bat
@@ -1,6 +1,13 @@
 REM Run the installer silently with full options
 set InstallerName=%1
 set InstallTargetPath=%2
+set InstallTargetPathLog=%2.log
+
 
 echo Installing package name %InstallerName%...
-"%InstallerName%" /D=%InstallTargetPath%
+"%InstallerName%" /S /D=%InstallTargetPath% /verbose > "%InstallTargetPathLog%" 2>&1
+
+REM Display the log file content
+echo ============================================================
+type "%InstallTargetPathLog%"
+echo ============================================================
