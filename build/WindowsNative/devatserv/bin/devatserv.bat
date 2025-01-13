1579a1e4a93c3e96fc4848bd18d03fdad3e33018
hvthong <50081052+huavanthong@users.noreply.github.com>
2025-01-13 16:55:13 +0700

    Update working directory for debug board service.

--- build/WindowsNative/devatserv/bin/devatserv.bat
+++ build/WindowsNative/devatserv/bin/devatserv.bat
@@ -2,9 +2,9 @@
 
 if "%1" == "start" (
     echo Starting DevAtServ...
-    start "Base Service" cmd /k "%DevAtServ%\python.exe" "%DEVATSERV_HOME%\python39\Lib\site-packages\MicroserviceBase\ServiceRegistry.py"
-    start "Cleware Switch Service" cmd /k "%DevAtServ%\python.exe" "%DEVATSERV_HOME%\python39\Lib\site-packages\MicroserviceClewareSwitch\ServiceCleware.py"
-    start "Debug Board Service" cmd /k python "%DevAtServ%\python.exe" "%DEVATSERV_HOME%\python39\Lib\site-packages\MicroserviceDebugboard\ServiceDebugboard.py"
+    start "Base Service" cmd /k ""%DevAtServ%\python.exe" "%DEVATSERV_HOME%\python39\Lib\site-packages\MicroserviceBase\ServiceRegistry\ServiceRegistry.py""
+    start "Cleware Switch Service" cmd /k ""%DevAtServ%\python.exe" "%DEVATSERV_HOME%\python39\Lib\site-packages\MicroserviceClewareSwitch\ServiceCleware.py""
+    start "Debug Board Service" /d "%DEVATSERV_HOME%\python39\Lib\site-packages\MicroserviceDebugboard" cmd /k ""%DevAtServ%\python.exe" "%DEVATSERV_HOME%\python39\Lib\site-packages\MicroserviceDebugboard\ServiceDebugboard.py" --use_remote_tools --remotetools_path="%DEVATSERV_HOME%\python39\Lib\site-packages\MicroserviceDebugboard\tools\remote_tools\windows\Gen5DBG_RemoteCtrl.exe""
 ) else if "%1" == "stop" (
     echo Stopping DevAtServ...
     @REM TODO in next action
--- build/WindowsNative/devatserv/bin/startup-devatserv.bat
+++ build/WindowsNative/devatserv/bin/startup-devatserv.bat
@@ -24,6 +24,6 @@ start "Base Service" cmd /k ""%DevAtServ%\python.exe" "%DEVATSERV_HOME%\python39
 echo Running Cleware Swtich Service
 start "Cleware Switch Service" cmd /k ""%DevAtServ%\python.exe" "%DEVATSERV_HOME%\python39\Lib\site-packages\MicroserviceClewareSwitch\ServiceCleware.py""
 echo Running Debug Board Service
-start "Debug Board Service" cmd /k ""%DevAtServ%\python.exe" "%DEVATSERV_HOME%\python39\Lib\site-packages\MicroserviceDebugboard\ServiceDebugboard.py" --use_remote_tools --remotetools_path="%DEVATSERV_HOME%\python39\Lib\site-packages\MicroserviceDebugboard\tools\remote_tools\windows\Gen5DBG_RemoteCtrl.exe""
+start "Debug Board Service" /d "%DEVATSERV_HOME%\python39\Lib\site-packages\MicroserviceDebugboard" cmd /k ""%DevAtServ%\python.exe" "%DEVATSERV_HOME%\python39\Lib\site-packages\MicroserviceDebugboard\ServiceDebugboard.py" --use_remote_tools --remotetools_path="%DEVATSERV_HOME%\python39\Lib\site-packages\MicroserviceDebugboard\tools\remote_tools\windows\Gen5DBG_RemoteCtrl.exe""
 
 pause
