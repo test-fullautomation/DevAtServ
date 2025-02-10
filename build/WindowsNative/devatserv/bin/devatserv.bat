545f0fc1e02c8192d98fa656e62f8011f4896a7e
hvthong <50081052+huavanthong@users.noreply.github.com>
2025-02-10 11:28:16 +0700

    Update script to manage all service.

--- build/WindowsNative/devatserv/bin/devatserv.bat
+++ build/WindowsNative/devatserv/bin/devatserv.bat
@@ -1,17 +1,49 @@
 @echo off
 
-if "%1" == "start" (
-    echo Starting DevAtServ...
-    start "Base Service" /B cmd /k ""%DevAtServ%\python.exe" "%DEVATSERV_HOME%\python39\Lib\site-packages\MicroserviceBase\ServiceRegistry\ServiceRegistry.py""
-    start "Cleware Switch Service" /B cmd /k ""%DevAtServ%\python.exe" "%DEVATSERV_HOME%\python39\Lib\site-packages\MicroserviceClewareSwitch\ServiceCleware.py""
-    start "Debug Board Service" /B /d "%DEVATSERV_HOME%\python39\Lib\site-packages\MicroserviceDebugboard" cmd /k ""%DevAtServ%\python.exe" "%DEVATSERV_HOME%\python39\Lib\site-packages\MicroserviceDebugboard\ServiceDebugboard.py" --use_remote_tools --remotetools_path="%DEVATSERV_HOME%\python39\Lib\site-packages\MicroserviceDebugboard\tools\remote_tools\windows\Gen5DBG_RemoteCtrl.exe""
-) else if "%1" == "stop" (
-    echo Stopping DevAtServ...
-    @REM TODO in next action
-    @REM taskkill /FI "WINDOWTITLE eq Base Service" /T /F
-    @REM taskkill /FI "WINDOWTITLE eq Cleware Switch Service" /T /F
-    @REM taskkill /FI "WINDOWTITLE eq Debug Board Service" /T /F
-) else (
-    echo Usage: %~nx0 [start|stop]
+REM Check if the DEVATSERV_HOME environment variable is set
+if "%DEVATSERV_HOME%" == "" (
+    echo ERROR: DEVATSERV_HOME environment variable is not set.
+    echo Please set DEVATSERV_HOME to the root directory of DevAtServ.
+    exit /b 1
 )
 
+REM Check if NSSM exists
+if not exist "%DEVATSERV_HOME%\share\nssm\nssm.exe" (
+    echo ERROR: NSSM not found at %DEVATSERV_HOME%\share\nssm\nssm.exe.
+    echo Please ensure NSSM is installed correctly.
+    exit /b 1
+)
+
+REM Handle command
+if "%1" == "start" (
+    echo Starting DevAtServ services...
+    "%DEVATSERV_HOME%\share\nssm\nssm.exe" start BaseService
+    "%DEVATSERV_HOME%\share\nssm\nssm.exe" start ClewareSwitchService
+    "%DEVATSERV_HOME%\share\nssm\nssm.exe" start DebugBoardService
+    echo All services started.
+) else if "%1" == "stop" (
+    echo Stopping DevAtServ services...
+    "%DEVATSERV_HOME%\share\nssm\nssm.exe" stop BaseService
+    "%DEVATSERV_HOME%\share\nssm\nssm.exe" stop ClewareSwitchService
+    "%DEVATSERV_HOME%\share\nssm\nssm.exe" stop DebugBoardService
+    echo All services stopped.
+) else if "%1" == "restart" (
+    echo Restarting DevAtServ services...
+    "%DEVATSERV_HOME%\share\nssm\nssm.exe" restart BaseService
+    "%DEVATSERV_HOME%\share\nssm\nssm.exe" restart ClewareSwitchService
+    "%DEVATSERV_HOME%\share\nssm\nssm.exe" restart DebugBoardService
+    echo All services restarted.
+) else if "%1" == "status" (
+    echo Restarting DevAtServ services...
+    "%DEVATSERV_HOME%\share\nssm\nssm.exe" status BaseService
+    "%DEVATSERV_HOME%\share\nssm\nssm.exe" status ClewareSwitchService
+    "%DEVATSERV_HOME%\share\nssm\nssm.exe" status DebugBoardService
+    echo All services restarted.
+) else (
+    echo Usage: %~nx0 [start|stop|restart|status]
+    echo Please specify a valid command.
+    exit /b 1
+)
+
+REM End script
+exit /b 0
--- build/WindowsNative/devatserv/bin/startup-devatserv.bat
+++ build/WindowsNative/devatserv/bin/startup-devatserv.bat
@@ -1,9 +1,11 @@
 @echo off
 
-echo [INFO] Start up all services...
-echo Running Base Service
-start "Base Service" cmd /k ""%DevAtServ%\python.exe" "%DEVATSERV_HOME%\python39\Lib\site-packages\MicroserviceBase\ServiceRegistry\ServiceRegistry.py""
-echo Running Cleware Swtich Service
-start "Cleware Switch Service" cmd /k ""%DevAtServ%\python.exe" "%DEVATSERV_HOME%\python39\Lib\site-packages\MicroserviceClewareSwitch\ServiceCleware.py""
-echo Running Debug Board Service
-start "Debug Board Service" /d "%DEVATSERV_HOME%\python39\Lib\site-packages\MicroserviceDebugboard" cmd /k ""%DevAtServ%\python.exe" "%DEVATSERV_HOME%\python39\Lib\site-packages\MicroserviceDebugboard\ServiceDebugboard.py" --use_remote_tools --remotetools_path="%DEVATSERV_HOME%\python39\Lib\site-packages\MicroserviceDebugboard\tools\remote_tools\windows\Gen5DBG_RemoteCtrl.exe""
+echo [INFO] Start up Remote Tool...
+start "" "%DEVATSERV_HOME%\python39\Lib\site-packages\MicroserviceDebugboard\tools\remote_tools\windows\Gen5DBG_RemoteCtrl.exe"
+
+@REM echo Running Base Service
+@REM start "Base Service" cmd /k ""%DevAtServ%\python.exe" "%DEVATSERV_HOME%\python39\Lib\site-packages\MicroserviceBase\ServiceRegistry\ServiceRegistry.py""
+@REM echo Running Cleware Swtich Service
+@REM start "Cleware Switch Service" cmd /k ""%DevAtServ%\python.exe" "%DEVATSERV_HOME%\python39\Lib\site-packages\MicroserviceClewareSwitch\ServiceCleware.py""
+@REM echo Running Debug Board Service
+@REM start "Debug Board Service" /d "%DEVATSERV_HOME%\python39\Lib\site-packages\MicroserviceDebugboard" cmd /k ""%DevAtServ%\python.exe" "%DEVATSERV_HOME%\python39\Lib\site-packages\MicroserviceDebugboard\ServiceDebugboard.py" --use_remote_tools --remotetools_path="%DEVATSERV_HOME%\python39\Lib\site-packages\MicroserviceDebugboard\tools\remote_tools\windows\Gen5DBG_RemoteCtrl.exe""
